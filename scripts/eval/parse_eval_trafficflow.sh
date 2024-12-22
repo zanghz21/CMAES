3da34f6ff3f6222134cf9d7f3ca2b326a2a85e48
zanghz <zanghzhi@gmail.com>
2024-12-22 09:16:41 -0500

    add parse modification

--- scripts/eval/parse_eval_trafficflow.sh
+++ scripts/eval/parse_eval_trafficflow.sh
@@ -1,6 +1,6 @@
 BASE_DIR=""
 TIME_STR=""
-N_EVALS=""
+N_EVALS=50
 
 SINGULARITY_OPTS="--cleanenv --env MALLOC_TRIM_THRESHOLD_=0"
 if [ -n "$PROJECT_DIR" ]; then
@@ -11,3 +11,8 @@ echo "Singularity opts: ${SINGULARITY_OPTS}"
 singularity exec ${SINGULARITY_OPTS} ../singularity/ubuntu_onlineGGO.sif \
     python env_search/traffic_mapf/parse_multi_process_eval.py \
     --base_dir=$BASE_DIR --time_str=$TIME_STR --n_evals=$N_EVALS
+  
+BASE_DIR=$BASE_DIR/logs
+
+singularity exec ${SINGULARITY_OPTS} ../singularity/ubuntu_onlineGGO.sif \
+    python env_search/traffic_mapf/parse_eval.py --base_dir=$BASE_DIR --time_str=$TIME_STR
